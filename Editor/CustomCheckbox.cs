fb5253a99959b15fb00b81a7e237356251466701
Administrator <ycw2416@outlook.com>
2025-09-19 13:49:15 +0800

    美化代码

--- Editor/CustomCheckbox.cs
+++ Editor/CustomCheckbox.cs
@@ -20,29 +20,32 @@ namespace Editor
             var obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
             if (obj == null || !obj.name.Contains("~")) return;
 
-            // 定义enable的checkbox的区域：放在右边
-            var activeToggleRect = new Rect(selectionRect.xMax, selectionRect.y, 18, selectionRect.height);
             // 获取状态
             var activeState = obj.activeInHierarchy;
-            // 绘制勾选框
-            var newActiveState = GUI.Toggle(activeToggleRect, activeState, GUIContent.none);
+            // 定义enable的checkbox的区域：放在右边
+            var newActiveState = GUI.Toggle(
+                new Rect(selectionRect.xMax, selectionRect.y, 18, selectionRect.height),
+                activeState,
+                GUIContent.none
+            );
             // 如果状态改变，更新
             if (newActiveState != activeState)
-            {
                 obj.SetActive(newActiveState);
-            }
 
             // 检测是否包含透明
             var objTranslucent = obj.GetComponent<ModelTranslucent>();
             if (objTranslucent == null) return;
-            // 定义透明的checkbox的区域：放在右边
+            // 获取状态
             var translucentState = objTranslucent.isTranslucnet;
-            var translucentToggleRect = new Rect(selectionRect.xMax - 18, selectionRect.y, 18, selectionRect.height);
-            var newTranslucentState = GUI.Toggle(translucentToggleRect, translucentState, GUIContent.none);
+            // 定义透明的checkbox的区域：放在右边
+            var newTranslucentState = GUI.Toggle(
+                new Rect(selectionRect.xMax - 18, selectionRect.y, 18, selectionRect.height),
+                translucentState,
+                GUIContent.none
+            );
+            // 如果状态改变，更新
             if (newTranslucentState != translucentState)
-            {
                 obj.GetComponent<ModelTranslucent>().SetTranslucentState(newTranslucentState);
-            }
         }
     }
 }
